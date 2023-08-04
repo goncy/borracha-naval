6c32700a5fea1b130d20faa6331b3c2dcd4e2bb6
goncy <gonzalo.pozzo4@gmail.com>
2023-08-03 21:42:52 -0300

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,30 @@ const MATRIX = Array.from({length: MATRIX_SIZE}, (_value, rowIndex) =>
   Array.from({length: MATRIX_SIZE}, (_value, colIndex) => [rowIndex, colIndex] as [number, number]),
 );
 
+function isAbove([aRow, aCol]: [number, number], [bRow, bCol]: [number, number]) {
+  return aRow === bRow - 1 && aCol === bCol;
+}
+
+function isBelow([aRow, aCol]: [number, number], [bRow, bCol]: [number, number]) {
+  return aRow === bRow + 1 && aCol === bCol;
+}
+
+function isLeft([aRow, aCol]: [number, number], [bRow, bCol]: [number, number]) {
+  return aCol === bCol - 1 && aRow === bRow;
+}
+
+function isRight([aRow, aCol]: [number, number], [bRow, bCol]: [number, number]) {
+  return aCol === bCol + 1 && aRow === bRow;
+}
+
 function getCellViability(cell: [number, number], ship: Set<[number, number]>) {
   if (ship.size === 0) return true;
   else if (ship.size >= SHIP_SIZE) return false;
   else if (ship.has(cell)) return false;
 
   const [cellRow, cellCol] = cell;
-  const [lastRow, lastCol] = Array.from(ship).at(-1)!;
+  const last = Array.from(ship).at(-1)!;
+  const [lastRow, lastCol] = last;
 
   // Check we can only click cells that allow a full ship to be built
   // ie: If I clicked [1,1] as first cell, I can't go to the left or top
@@ -35,8 +52,9 @@ function getCellViability(cell: [number, number], ship: Set<[number, number]>) {
 
   // Check that we only click contiguous cells, horizontally or vertically
   if (ship.size === 1) {
-    if (cellRow === firstRow && (cellCol === firstCol - 1 || cellCol === firstCol + 1)) return true;
-    if (cellCol === firstCol && (cellRow === firstRow - 1 || cellRow === firstRow + 1)) return true;
+    if (isLeft(cell, last) || isRight(cell, last) || isAbove(cell, last) || isBelow(cell, last)) {
+      return true;
+    }
   }
 
   // If we already decided a direction, we can only click cells in that direction
@@ -60,30 +78,73 @@ function getCellViability(cell: [number, number], ship: Set<[number, number]>) {
 
 export default function Home() {
   const [ship, setShip] = useState<Set<[number, number]>>(() => new Set());
+  const shipIsReady = ship.size === SHIP_SIZE;
+
+  function handleCellClick(cell: [number, number]) {
+    const draft = new Set(ship);
+
+    draft.add(cell);
+
+    setShip(draft);
+  }
+
+  function handleRemoveLastPiece() {
+    const draft = new Set(ship);
+    const last = Array.from(ship).at(-1)!;
+
+    draft.delete(last);
+
+    setShip(draft);
+  }
 
   return (
-    <main className="flex w-fit flex-col border">
-      {MATRIX.map((row, rowIndex) => (
-        <div key={rowIndex} className="flex">
-          {row.map((cell, colIndex) => {
-            const isViable = getCellViability(cell, ship);
+    <main className="grid gap-4">
+      <section className="flex w-fit flex-col border">
+        {MATRIX.map((row, rowIndex) => (
+          <div key={rowIndex} className="flex">
+            {row.map((cell, colIndex) => {
+              const isViable = getCellViability(cell, ship);
 
-            return (
-              <div
-                key={colIndex}
-                className={`grid h-16 w-16 place-content-center border ${
-                  ship.has(cell) ? "bg-green-500" : "bg-transparent"
-                } ${isViable ? "cursor-pointer" : "cursor-not-allowed"}`}
-                onClick={() => isViable && setShip(new Set(ship.add(cell)))}
-              >
-                <span>
-                  {rowIndex}, {colIndex}
-                </span>
-              </div>
-            );
-          })}
-        </div>
-      ))}
+              return (
+                <div
+                  key={colIndex}
+                  className={`grid h-16 w-16 place-content-center border ${
+                    ship.has(cell) ? "bg-green-500" : "bg-transparent"
+                  } ${isViable ? "cursor-pointer" : "cursor-not-allowed"}`}
+                  onClick={() => isViable && handleCellClick(cell)}
+                >
+                  <span>
+                    {rowIndex}, {colIndex}
+                  </span>
+                </div>
+              );
+            })}
+          </div>
+        ))}
+      </section>
+      <ul className="grid grid-flow-col place-content-between gap-4">
+        {ship.size > 0 && (
+          <li>
+            <button className="rounded border p-4" type="button" onClick={handleRemoveLastPiece}>
+              Delete last piece
+            </button>
+          </li>
+        )}
+        {Boolean(shipIsReady) && (
+          <>
+            <li>
+              <button className="rounded border p-4" type="button">
+                Rotate clockwise
+              </button>
+            </li>
+            <li>
+              <button className="rounded border p-4" type="button">
+                Rotate anti-clockwise
+              </button>
+            </li>
+          </>
+        )}
+      </ul>
     </main>
   );
 }
